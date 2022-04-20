334b174ba221b0cb4fa8952237c049c24afb3e61
marshall-ku <72745119+marshall-ku@users.noreply.github.com>
2022-04-20 17:33:56 +0900

    Update commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [[ `git status --porcelain` ]]; then
+    git config user.name "github-actions"
+    git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
     git add -A
     git commit -m "$MESSAGE"
     git push
